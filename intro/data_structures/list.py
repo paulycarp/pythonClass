e08d088c97e014383b9af8c7e32ce6a03d7fe27d
paulycarp <stev.paul@yahoo.com>
2022-10-26 12:32:07 +0100

    class in progress

--- intro/data_structures/list.py
+++ intro/data_structures/list.py
@@ -1,4 +1,5 @@
 # Lists
+print("\n******** Working with List ********\n")
 
 letters = ["a", "b", "c", "d", "e", "f", "g", "h"]
 num = [[1, 4], [2, 8]]
@@ -8,6 +9,7 @@ print(twos, num)
 print(type(num))
 
 # Concatination
+print("\n******** Concatinate ********\n")
 alphanum = letters + twos # This line of code helps us to concatinate two lists
 print(alphanum)
 
@@ -16,9 +18,11 @@ print(list(numbers))
 print(list('classified'))
 
 # Lenght
+print("\n******** Lenght of a string ********\n")
 len(alphanum)
 
 # Accessing List
+print("\n******** Accessing List item ********\n")
 # We use [] to access list
 
 alphanum[5]
@@ -37,7 +41,7 @@ print((nums[::-1]))
 print((nums[::3])) # Lists range of numbers in step of 2 (even numbers) without listing them explicitly
 
 # unpacking list
-
+print("\n******** Lsit unpacking ********\n")
 number1 = [1, 2, 3]
 frist = number1[0]
 second = number1[1]
@@ -50,6 +54,7 @@ number3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 first, second, third, *other = number2
 
 # Looping
+print("\n******** Working with Loop ********\n")
 for letter in letters:
     print(letters)
 
@@ -60,7 +65,7 @@ for index, letter in enumerate(letters):
     print(index, letter)
 
 # Adding and Removing items into and from a list
-
+print("\n******** Adding and Removing item from a list ********\n")
 letter1 = ["a", "b", "c", "f", "g"]
 #Add
 letter1.append("d")
@@ -112,11 +117,89 @@ numSorted = sorted(num)
 print(numSorted)
 
 #Complex Sorting
-item = [
+items = [
     ("Produce1", 45),
     ("Produce2", 15),
     ("Produce3", 10),
 ]
 
-item.sort()
+items.sort()
 print()
+
+# create a sort function
+def sort_item(item):
+    return item[1]
+
+items.sort(key=sort_item)
+items.sort(key=sort_item, reverse=True)
+print(items)
+
+# Lambda Functions
+print("\n******** Working with Lambda ********\n")
+# This is a one line anonymous function we can pass to list method
+# items.sort(key=labda parameter: expression) --> basic lambda syntax
+
+items.sort(key=lambda item: item[1])
+print(items)
+
+# Map Function
+print("\n******** Working with Map ********\n")
+items = [
+    ("Produce1", 45),
+    ("Produce2", 15),
+    ("Produce3", 10),
+]
+
+prices = []
+for item in items:
+    prices.append(item)
+print(prices)
+
+# Map
+prices = list(map(lambda item:item[1], items))
+print(prices)
+
+prices = list(map(lambda item:item * 3, [2, 5, 8, 11]))
+print(prices)
+
+# Filter
+# Selecting items base on condition
+print("\n******** Working with Filter ********\n")
+
+items = [
+    ("Produce1", 45),
+    ("Produce2", 15),
+    ("Produce3", 10),
+]
+
+# get all items with price greater than or equal to 10
+
+filtered = []
+for item in items:
+    if item[1] >= 10:
+        filtered.append(items[0])
+        #filtered.append(items)
+print(filtered)
+
+filtered = list(filter(lambda item:item[1] >= 10, items))
+print(filtered)
+
+# List comprehension
+print("\n******** Working with List Comprehension ********\n")
+# Syntax [expression for item in items]
+# Map
+prices = list(map(lambda item:item[1], items))
+print(prices)
+prices = [items[1] for item in items]
+print(prices)
+
+#Filtere
+filtered = list(map(lambda item:item[1], items))
+print(filtered)
+prices = [items[0] for item in items if item[1] >= 10]
+print(filtered)
+
+# ZIP Function
+# 
+
+
